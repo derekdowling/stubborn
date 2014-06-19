4fe06c437764090c0d6e1abe91d748b8ede85341
derekdowling <thederekdowling@gmail.com>
2014-06-19 10:47:33 -0600

    Updating composer

--- vendor/autoload.php
+++ vendor/autoload.php
@@ -4,4 +4,4 @@
 
 require_once __DIR__ . '/composer' . '/autoload_real.php';
 
-return ComposerAutoloaderInit3f3251ab4872ec081fd54030ea4c6785::getLoader();
+return ComposerAutoloaderInitfb336870a744007e97ddd17adec3c9ff::getLoader();
